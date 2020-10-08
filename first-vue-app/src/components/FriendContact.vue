98015471a9bf9868c79d40e3e1004e9e437c69ea
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2020-10-08 21:57:09 +0200

    component communication

--- first-vue-app/src/components/FriendContact.vue
+++ first-vue-app/src/components/FriendContact.vue
@@ -1,30 +1,75 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetails">Show details</button>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} details
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong>{{ friend.phone }}</li>
-      <li><strong>Email:</strong>{{ friend.email }}</li>
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong>{{ emailAddress }}</li>
     </ul>
+    <button @click="$emit('delete', id)">Delete</button>
   </li>
 </template>
 
 <script>
 export default {
+  // props can be used like data properties in js. eg: this.name
+  // camel-case notation here. in html kebap-case
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  // provide type for validation
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      // default can take function as well
+      default: false,
+      // validator: function (value) {
+      //   return value === "1" || value === "0";
+      // },
+    },
+  },
+  emits: ["toggle-favorite", "delete"],
+  // emits: {
+  //   "toggle-favorite": function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("Id is missing.");
+  //       return false;
+  //     }
+  //   },
+  // },
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: "kilian",
-        name: "Kilian Kalus",
-        phone: "934 840 2324",
-        email: "kilian.kalus@gmail.com",
-      },
+      // dont mutate props. store in variable then work with it
+      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
     },
   },
 };
